4649bbff9474a52afcb1973dbf17d5d1b944fc78
BARBERBLAD\jeppe <Jeppefriislund@hotmail.dk>
2026-08-14 11:23:44 +0200

    Added Posts from the api

---
+++ src/Posts.tsx
@@ -0,0 +1,83 @@
+import {useEffect, useState} from "react";
+
+export function ListPosts() {
+
+    const [posts, setPosts] = useState<Post[]>([])
+
+    useEffect(() => {
+        fetch('https://dummyjson.com/posts')
+            .then(res => res.json())
+            .then((json) => {
+                setPosts(json.products)
+            });
+    }, []);
+
+    function removePost(id: number) {
+        const duplicate = [...posts];
+        const filteredArray = duplicate.filter(p => p.id != id)
+        setPosts(filteredArray)
+    }
+
+    function renameProduct(id: number, newTitle: string) {
+        setPosts(posts.map(p =>
+            p.id === id ? {...p, title: newTitle} : p
+        ));
+    }
+
+    function seeProduct(id: number) {
+        const duplicate = [...posts];
+        const filteredArray = duplicate.filter(p => p.id == id)
+        setPosts(filteredArray)
+    }
+
+
+    return <div>
+        {
+            posts.map(p => {
+                return <div>
+                    <MyChildComponent key={p.id} post={p}
+                                      removePost={removePost}
+
+                    />
+                </div>
+
+
+            })
+        }
+    </div>;
+}
+
+interface MyChildComponentProps {
+    post: Post,
+    removePost: (id: number) => void
+}
+
+function MyChildComponent({post, removePost,}: MyChildComponentProps) {
+
+
+    return <div>the product is: {post?.title} <button onClick={() => removePost(post.id)}>delete this stuff</button></div>
+
+}
+
+
+export interface Root {
+    posts: Post[]
+    total: number
+    skip: number
+    limit: number
+}
+
+export interface Post {
+    id: number
+    title: string
+    body: string
+    tags: string[]
+    reactions: Reactions
+    views: number
+    userId: number
+}
+
+export interface Reactions {
+    likes: number
+    dislikes: number
+}
