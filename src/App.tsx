b98f1476c06446c70fab75fc6054ab05da50b1f5
BARBERBLAD\jeppe <Jeppefriislund@hotmail.dk>
2026-08-17 10:06:34 +0200

    Added some css styling so it looks like X

--- src/App.tsx
+++ src/App.tsx
@@ -18,21 +18,6 @@ const myRoutes: RouteObject[] = [
 export function App() {
 
     return <RouterProvider router={createBrowserRouter(myRoutes)} />
-
-    return (
-        <div className="app">
-            <div className="logo-container">
-                <img src={logo} alt="Bun Logo" className="logo bun-logo" />
-                <img src={reactLogo} alt="React Logo" className="logo react-logo" />
-            </div>
-
-            <h1>Bun + React</h1>
-            <p>
-                Edit <code>src/App.tsx</code> and save to test HMR
-            </p>
-            <APITester />
-        </div>
-    );
 }
 
 export default App;
