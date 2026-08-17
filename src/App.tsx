ef526bc636a63cd4b7ad67b8b862a78a9b628d76
BARBERBLAD\jeppe <Jeppefriislund@hotmail.dk>
2026-08-17 10:22:59 +0200

    Added Layout.tsx to center everything

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,19 @@ import {createBrowserRouter, type RouteObject, RouterProvider} from "react-route
 import logo from "./logo.svg";
 import reactLogo from "./react.svg";
 import {Home} from "@/Home.tsx";
+import { Layout } from "./Layout";
 
 import { ListPosts } from "./Posts.tsx";
 
 const myRoutes: RouteObject[] = [
-    { path: "/", element: <Home /> },
-    { path: "/register", element: <APITester /> },
-    { path: "/posts", element: <ListPosts /> },
+    {
+        element: <Layout />,
+        children: [
+            { path: "/", element: <Home /> },
+            { path: "/register", element: <APITester /> },
+            { path: "/posts", element: <ListPosts /> },
+        ],
+    },
 ]
 
 export function App() {
