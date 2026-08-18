41478c5416a39ee73da3530e8935ba975317a39d
BARBERBLAD\jeppe <Jeppefriislund@hotmail.dk>
2026-08-18 09:33:33 +0200

    Changed the layout to three columns + added logo and create post button

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,9 +1,27 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
+import logo from "./logo.svg";
 
 export function Layout() {
+    const location = useLocation();
+    const isFeedPage = location.pathname === "/posts";
+
     return (
-        <div className="layout">
-            <Outlet />
+        <div className="app-shell">
+            <aside className="sidebar-left">
+                {isFeedPage && (
+                    <>
+                        <img src={logo} alt="Logo" className="sidebar-logo" />
+                        <button>Create post</button>
+                    </>
+                )}
+            </aside>
+
+            <main className="feed-column">
+                <Outlet />
+            </main>
+
+            <aside className="sidebar-right">
+            </aside>
         </div>
     );
 }
